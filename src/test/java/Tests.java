d56d066b42282e961d754e6269abbc76307e3d90
Roman Bulinski <aboandrus@gmail.com>
2019-03-24 15:07:11 +0100

    Added new test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -10,9 +10,13 @@ public class Tests {
         Main main = new Main();
         String result = "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu";
         assertEquals(result, main.accum("ZpglnRxqenU") );
-
     }
 
-
+    @Test
+    public void test1() {
+        Main main = new Main();
+        String result = "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc";
+        assertEquals(result, main.accum("HbideVbxncC") );
+    }
 
 }
